10e42e6244fa9d9df4ed9a83f9cac67236b9060b
lavanyanidamanuri555 <113895722+lavanyanidamanuri555@users.noreply.github.com>
2024-12-23 18:15:10 +0530

    Guest.java

--- online hotel booking/Guest.java
+++ online hotel booking/Guest.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 public class Guest {
     private int guestId;
     private String name;
     private String contactInfo;
     private String passportOrNationalId;
     private String email;
+    private List<Reservation> reservations;  
 
     public Guest(int guestId, String name, String contactInfo, String passportOrNationalId, String email) {
         this.guestId = guestId;
@@ -11,6 +14,7 @@ public class Guest {
         this.contactInfo = contactInfo;
         this.passportOrNationalId = passportOrNationalId;
         this.email = email;
+        this.reservations = new ArrayList<>();
     }
 
     public int getGuestId() {
@@ -21,15 +25,16 @@ public class Guest {
         return name;
     }
 
-    public String getContactInfo() {
-        return contactInfo;
+    public List<Reservation> getReservations() {
+        return reservations;
     }
 
-    public String getPassportOrNationalId() {
-        return passportOrNationalId;
+    public void addReservation(Reservation reservation) {
+        this.reservations.add(reservation);
     }
 
-    public String getEmail() {
-        return email;
+    @Override
+    public String toString() {
+        return "Guest ID: " + guestId + ", Name: " + name;
     }
 }
